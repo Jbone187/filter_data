ee089c25522b11aa42d9d671899580330aca27b3
Jasen Green <jasen.green@protonmail.com>
2021-05-04 13:58:10 -0700

    Update file

--- file_filter.js
+++ file_filter.js
@@ -1,7 +1,10 @@
-let fs = require('fs')
+let fs = require('fs');
+
+let crush_node_config = require('./crush_node_config.json');
 
 
-fs.readFile('hello2.txt', 'utf8', function (err, data) {
+fs.readFile('crush_node_config.file', 'utf8', function (err, data) {
+
     if (err) {
         return console.log(err);
     };
@@ -12,7 +15,7 @@ fs.readFile('hello2.txt', 'utf8', function (err, data) {
 
     let arrFiltered = data_split.filter(Boolean);
 
-    console.log(arrFiltered)
+    console.log(arrFiltered);
 
 
     let crush_obj = {
@@ -22,7 +25,6 @@ fs.readFile('hello2.txt', 'utf8', function (err, data) {
 
     let dataObject = JSON.stringify(crush_obj);
 
-    fs.writeFileSync("/home/jasen/Node/new_crush_data.json", dataObject);
-
+    fs.writeFileSync(crush_node_config.jsonFile, dataObject);
 
 });
