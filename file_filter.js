1fa45e32dcd028c6dcc554ff48df1422b27a29dc
Jasen Green <jasen.green@protonmail.com>
2021-05-04 13:51:26 -0700

    Update file_filter.js

--- file_filter.js
+++ file_filter.js
@@ -1,46 +1,28 @@
 let fs = require('fs')
 
-let arr = []
 
-fs.readFile('hello2.txt', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  };
+fs.readFile('hello2.txt', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    };
 
-  let new_Data = data.replace(/\r?\n|\r/g, " ").replace(/[ ]+/g, " ");
+    let new_Data = data.replace(/\r?\n|\r/g, " ").replace(/[ ]+/g, " ");
 
-  console.log(new_Data)
+    let data_split = new_Data.split(" ");
 
+    let arrFiltered = data_split.filter(Boolean);
 
-  let data_split = new_Data.split(" ");
-
-  console.log(data_split);
-  
-
- let arrFiltered = data_split.filter(Boolean);
-
- console.log(arrFiltered)
+    console.log(arrFiltered)
 
 
- let crush_obj = {
+    let crush_obj = {
 
-    crush: [arrFiltered]
-};
+        crush: [arrFiltered]
+    };
 
-let dataObject = JSON.stringify(crush_obj);
+    let dataObject = JSON.stringify(crush_obj);
 
-fs.writeFileSync("/home/jasen/Node/new_crush_data.json", dataObject);
-
-
-/*
-  
-  for(let i=0; i < arrFiltered.length; i++){
-
-    arr.push(arrFiltered[i])
-  };
+    fs.writeFileSync("/home/jasen/Node/new_crush_data.json", dataObject);
 
 
-*/
-
-  
 });
